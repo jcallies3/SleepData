19aa45b1d97878503af438a3e49634bc3631c0ff
jcallies3 <jcallies3@my.wctc.edu>
2025-09-02 13:40:32 -0500

    [1.01] Add user input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//ask for input
+Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data file.");
+Console.WriteLine("Emter anything else to quit.");
+// input response
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    // TODO: create file
+}
+if (resp == "2")
+{
+    // TODO : parse data file
+}
